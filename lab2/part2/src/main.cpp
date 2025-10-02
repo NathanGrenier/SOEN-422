5525bf841013eaa0bb70511fd825235964771382
Nathan Grenier <nathangrenier01@gmail.com>
2025-10-02 16:34:13 -0400

    feat: BLE server broadcasting and service / characteristic writing

--- lab2/part2/src/main.cpp
+++ lab2/part2/src/main.cpp
@@ -4,8 +4,9 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <BLEDevice.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 #include "credentials.h"
 #include "api_config.h"
@@ -14,65 +15,120 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-const char *ssid = WIFI_SSID;
-const char *password = WIFI_PASSWORD;
+// Go to https://www.uuidgenerator.net to generate unique UUIDs
+#define SERVICE_UUID "d81f1e40-a43c-4c07-b83f-71d584802b4e"
+#define CHARACTERISTIC_UUID "4e44cef2-ed01-4edf-952d-25d88fe182a3"
+
+const char *SSID = WIFI_SSID;
+const char *PASSWORD = WIFI_PASSWORD;
+uint32_t PASSKEY = BLE_PASSKEY;
 
 const unsigned int STUDENT_ID = 40250986;
 
-const String BLE_NAME_PHONE1[] = {"Yuma1", "doom"};
-const String BLE_NAME_PHONE2[] = {"Yuma2", "starwars"};
-const String VALID_BLE_NAMES[] = {BLE_NAME_PHONE1[0], BLE_NAME_PHONE2[0]};
-String currentBTAddress;
-String currentDeviceName;
-
 const unsigned char BUZZER_PIN = 21;
 
-BLEScan *pBLEScan;
-volatile bool isScanning = false;
-volatile bool isProcessing = false;
+volatile bool isDeviceConnected = false;
+volatile bool isDeviceAuthenticated = false;
 
-bool postUserSongPreference(unsigned int studentId, const String &bluetoothAddress, const String &songName);
-
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+class MyServerCallbacks : public BLEServerCallbacks
 {
-  void onResult(BLEAdvertisedDevice advertisedDevice)
+  void onConnect(BLEServer *pServer)
   {
-    String deviceAddress = advertisedDevice.getAddress().toString().c_str();
-    String deviceName = advertisedDevice.haveName() ? advertisedDevice.getName().c_str() : "N/A";
-    Serial.printf("Found Device: Address: %s, Name: %s, RSSI: %d\n",
-                  deviceAddress.c_str(),
-                  deviceName.c_str(),
-                  advertisedDevice.getRSSI());
+    isDeviceConnected = true;
+    Serial.println("Device connected");
+  };
 
-    // Do not process if we are already handling a device
-    if (isProcessing)
+  void onDisconnect(BLEServer *pServer)
+  {
+    isDeviceConnected = false;
+    isDeviceAuthenticated = false;
+    Serial.println("Device disconnected");
+    // Restart advertising to allow new connections
+    pServer->getAdvertising()->start();
+    Serial.println("Restarting advertising...");
+  }
+};
+
+class MySecurityCallbacks : public BLESecurityCallbacks
+{
+  // This is called when the pairing process is complete.
+  void onAuthenticationComplete(esp_ble_auth_cmpl_t auth_cmpl)
+  {
+    if (auth_cmpl.success)
     {
-      return;
+      isDeviceAuthenticated = true;
+      Serial.println("Pairing successful. Connection fully established.");
     }
-
-    bool isValidDevice = false;
-    for (const String &validName : VALID_BLE_NAMES)
+    else
     {
-      if (deviceName.equals(validName))
+      isDeviceAuthenticated = false;
+      Serial.println("Authentication failed.");
+    }
+  }
+
+  // The ESP32 will ask for a passkey to be entered on the client device.
+  // This callback is not strictly needed for static passkey but is good practice.
+  uint32_t onPassKeyRequest()
+  {
+    return PASSKEY;
+  }
+
+  // This is not used in the static passkey scenario but must be provided.
+  void onPassKeyNotify(uint32_t pass_key) {}
+
+  bool onConfirmPIN(uint32_t pass_key)
+  {
+    return true;
+  }
+
+  // This is not used in the static passkey scenario but must be provided.
+  bool onConfirmRequest(uint32_t pass_key)
+  {
+    return true;
+  }
+
+  // This is not used in the static passkey scenario but must be provided.
+  bool onSecurityRequest()
+  {
+    return true;
+  }
+};
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+    String command = value.c_str();
+
+    if (value.length() > 0)
+    {
+      switch (value[0])
       {
-        isValidDevice = true;
+      // Play/Pause
+      case '1':
+        Serial.println("-> Play/Pause command received.");
+        break;
+      // Next Song
+      case '2':
+        Serial.println("-> Next Song command received.");
+        break;
+      // Previous Song
+      case '3':
+        Serial.println("-> Previous Song command received.");
+        break;
+      default:
+        Serial.println("-> Unknown command: " + command);
         break;
       }
-    }
-
-    if (isValidDevice)
-    {
-      currentBTAddress = deviceAddress;
-      currentDeviceName = deviceName;
-      isProcessing = true;
     }
   }
 };
 
 void connectToWifi()
 {
-  Serial.printf("Connecting to %s ", ssid);
-  if (strcmp(ssid, "SOEN422") == 0)
+  Serial.printf("Connecting to %s ", SSID);
+  if (strcmp(SSID, "SOEN422") == 0)
   {
     IPAddress local_IP(172, 30, 140, 210);
     IPAddress gateway(172, 30, 140, 129);
@@ -81,12 +137,20 @@ void connectToWifi()
     IPAddress secondaryDNS(8, 8, 4, 4);
     WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS);
   }
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
+
+  unsigned long startTime = millis();
   while (WiFi.status() != WL_CONNECTED)
   {
+    if (millis() - startTime > 15000) // 15 second timeout
+    {
+      Serial.println("\nFailed to connect to WiFi. Please check credentials or network.");
+      return;
+    }
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
@@ -106,29 +170,50 @@ void setup()
   // Free Classic BT controller memory (we only use BLE)
   esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
 
-  // Good Website for Master -> Slave BT Communication: // https://deepbluembedded.com/esp32-bluetooth-classic-with-arduino-complete-guide/
+  // Create the BLE Device
+  BLEDevice::init("ESP32_MusicPlayer_" + std::to_string(STUDENT_ID));
 
-  // Good Website for ESP32 Interrupts: https://deepbluembedded.com/esp32-timers-timer-interrupt-tutorial-arduino-ide/
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
 
-  BLEDevice::init("ESP32_40250986");
-  pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true); // active scan uses more power, but gets results faster
-  pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99);
+  // Create BLE Security Callbacks
+  BLEDevice::setSecurityCallbacks(new MySecurityCallbacks());
+  // Set up security
+  BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_MITM);
+  esp_ble_auth_req_t auth_req = ESP_LE_AUTH_REQ_SC_MITM_BOND;
+  esp_ble_io_cap_t iocap = ESP_IO_CAP_OUT; // Display a PIN
+  esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_STATIC_PASSKEY, &PASSKEY, sizeof(uint32_t));
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristic->setAccessPermissions(ESP_GATT_PERM_WRITE_ENCRYPTED);
+  // Add a descriptor to name the characteristic
+  BLEDescriptor *pDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
+  pDescriptor->setValue("Music Control");
+  pDescriptor->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED);
+  pCharacteristic->addDescriptor(pDescriptor);
+
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->start();
+  Serial.println("Advertising Started. Waiting for a client connection...");
 
   // Connect to Wi-Fi
   connectToWifi();
-
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("WiFi connection failed.");
-  }
-
-  // Start BLE Scan
-  Serial.println("Starting BLE scan...");
-  pBLEScan->start(0, nullptr, false); // Scan indefinitely, don't block, and don't store results after callback
-  isScanning = true;
 }
 
 /**
@@ -137,7 +222,7 @@ void setup()
  * @param doc A reference to a JsonDocument to store the parsed JSON data.
  * @return true if the song was fetched and parsed successfully, false otherwise.
  */
-bool getSongJson(const String &songName, JsonDocument &doc)
+bool getSongJsonByName(const String &songName, JsonDocument &doc)
 {
   if (WiFi.status() != WL_CONNECTED)
   {
@@ -150,7 +235,7 @@ bool getSongJson(const String &songName, JsonDocument &doc)
 
   HTTPClient http;
   http.useHTTP10(true);
-  http.begin(secureClient, Api::UrlBuilder::getSong(songName));
+  http.begin(secureClient, Api::UrlBuilder::getSongByName(songName));
 
   int httpResponseCode = http.GET();
   if (httpResponseCode != HTTP_CODE_OK)
@@ -179,13 +264,11 @@ bool getSongJson(const String &songName, JsonDocument &doc)
 }
 
 /**
- * @brief Fetches the user's song preference from the API.
- * @param studentId The student's ID.
- * @param bluetoothAddress The Bluetooth address of the device.
+ * @brief Fetches song data from the API and parses it into a JsonDocument.
  * @param doc A reference to a JsonDocument to store the parsed JSON data.
- * @return true if the preference was fetched and parsed successfully, false otherwise.
+ * @return true if the song was fetched and parsed successfully, false otherwise.
  */
-bool getUserSongPreference(unsigned int studentId, const String &bluetoothAddress, JsonDocument &doc)
+bool getSongJson(JsonDocument &doc)
 {
   if (WiFi.status() != WL_CONNECTED)
   {
@@ -198,7 +281,7 @@ bool getUserSongPreference(unsigned int studentId, const String &bluetoothAddres
 
   HTTPClient http;
   http.useHTTP10(true);
-  http.begin(secureClient, Api::UrlBuilder::getPreference(studentId, bluetoothAddress));
+  http.begin(secureClient, Api::UrlBuilder::getSong());
 
   int httpResponseCode = http.GET();
   if (httpResponseCode != HTTP_CODE_OK)
@@ -222,53 +305,6 @@ bool getUserSongPreference(unsigned int studentId, const String &bluetoothAddres
     Serial.println(error.f_str());
     return false;
   }
-
-  return true;
-}
-
-/**
- * @brief Posts the user's song preference to the API.
- * @param studentId The student's ID.
- * @param bluetoothAddress The Bluetooth address of the device.
- * @param songName The name of the song to set as preference.
- * @return true if the preference was posted successfully, false otherwise.
- */
-bool postUserSongPreference(unsigned int studentId, const String &bluetoothAddress, const String &songName)
-{
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("WiFi Disconnected, cannot post song preference.");
-    return false;
-  }
-
-  WiFiClientSecure secureClient;
-  secureClient.setInsecure();
-
-  HTTPClient http;
-  http.begin(secureClient, Api::UrlBuilder::postPreference(studentId, bluetoothAddress, songName));
-
-  // No body needed for this POST request
-  int httpResponseCode = http.POST("");
-  if (httpResponseCode != HTTP_CODE_OK && httpResponseCode != HTTP_CODE_CREATED)
-  {
-    String response = http.getString();
-    if (response.length() > 120)
-    {
-      response = response.substring(0, 120) + "...";
-    }
-    Serial.printf("Error [%d]: %s\n", httpResponseCode, response.c_str());
-    http.end();
-    return false;
-  }
-
-  String response = http.getString();
-
-  if (httpResponseCode == HTTP_CODE_OK && response.length() > 0)
-  {
-    Serial.println("Response from server: " + response);
-  }
-
-  http.end();
 
   return true;
 }
@@ -331,91 +367,5 @@ void loop()
   {
     Serial.println("WiFi Disconnected, trying to reconnect...");
     connectToWifi();
-  }
-
-  // If we are not busy processing a device and the scan is not running, start it.
-  if (!isProcessing && !isScanning)
-  {
-    Serial.println("Restarting BLE scan...");
-    pBLEScan->start(0, nullptr, false);
-    isScanning = true;
-  }
-
-  if (isProcessing)
-  {
-    // This is a safeguard against any lingering race conditions.
-    if (isScanning)
-    {
-      pBLEScan->stop();
-      isScanning = false;
-    }
-
-    String songToSet = "";
-    if (currentDeviceName.equalsIgnoreCase(BLE_NAME_PHONE1[0]))
-    {
-      songToSet = BLE_NAME_PHONE1[1];
-    }
-    else if (currentDeviceName.equalsIgnoreCase(BLE_NAME_PHONE2[0]))
-    {
-      songToSet = BLE_NAME_PHONE2[1];
-    }
-
-    Serial.println("Setting song preference to '" + songToSet + "' for device " + currentBTAddress);
-    if (postUserSongPreference(STUDENT_ID, currentBTAddress, songToSet))
-    {
-      Serial.println("Successfully set song preference.");
-
-      // Now fetch and play the song
-      Serial.println("Fetching user song preference...");
-      if (getUserSongPreference(STUDENT_ID, currentBTAddress, preferenceDoc))
-      {
-        Serial.println("Successfully fetched user song preference: ");
-        serializeJsonPretty(preferenceDoc, Serial);
-        Serial.println();
-        songName = preferenceDoc["name"].as<String>();
-      }
-      else
-      {
-        Serial.println("Failed to retrieve user song preference.");
-        songName = "";
-      }
-
-      if (songName != "")
-      {
-        Serial.println("Fetching user's preferred song data...");
-        if (getSongJson(songName, songDoc))
-        {
-          Serial.println("Successfully fetched song: ");
-          serializeJsonPretty(songDoc, Serial);
-          Serial.println();
-
-          Serial.println("Playing song: " + songName);
-          JsonArray melody = songDoc["melody"].as<JsonArray>();
-          unsigned char tempo = songDoc["tempo"].as<unsigned char>();
-          playSong(melody, tempo);
-        }
-        else
-        {
-          Serial.println("Failed to retrieve song data.");
-        }
-      }
-      else
-      {
-        Serial.println("No song preference set for this device.");
-      }
-    }
-    else
-    {
-      Serial.println("Failed to set song preference.");
-    }
-
-    // Reset state to allow for a new scan, regardless of success or failure
-    Serial.println("Processing complete. Resetting for next scan.");
-    currentBTAddress = "";
-    currentDeviceName = "";
-    songName = "";
-    preferenceDoc.clear();
-    songDoc.clear();
-    isProcessing = false;
   }
 }
