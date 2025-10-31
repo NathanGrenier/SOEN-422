372b10db779facfbf1946664928ce8c364eeda57
NathanGrenier <nathangrenier01@gmail.com>
2025-10-31 17:11:29 -0400

    docs: updated lab3 credentials example file with mobile hotspot

--- lab3/part2/include/credentials.example.h
+++ lab3/part2/include/credentials.example.h
@@ -1,7 +1,12 @@
 #pragma once
 
+// Lab WiFi Credentials
 #define WIFI_SSID "YOUR_SSID"
 #define WIFI_PASSWORD "YOUR_PASSWORD"
+
+// Mobile Hotspot Credentials
+// #define WIFI_SSID "YOUR_SSID"
+// #define WIFI_PASSWORD "YOUR_PASSWORD"
 
 #define ROOM_USERNAME "user"
 #define ROOM_PASSWORD "password"
